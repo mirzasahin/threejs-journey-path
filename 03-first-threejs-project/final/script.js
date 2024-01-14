6f008acbee54d5db2a563cd8a22c5e243071dfcc
Mirza Sahin <salihmirza123@gmail.com>
2024-01-14 14:05:25 +0300

    Create firt 3D scene with WebGL

---
+++ 03-first-threejs-project/final/script.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial( {color: '#ff0000'} )
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Sizes
+const size = {
+    width: 800,
+    height: 600
+}
+
+// Camera
+const camera = new THREE.PerspectiveCamera(35, size.width / size.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas // If they have same name we can use just "canvas" without second canvas text
+})
+renderer.setSize(size.width, size.height)
+
+renderer.render(scene, camera)
