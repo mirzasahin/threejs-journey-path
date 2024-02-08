15bf4751186a65a6ee04a4d0067cfd04fe41536c
Mirza Sahin <salihmirza123@gmail.com>
2024-02-08 09:46:42 +0300

    Add new material variations

--- 11-materials/exercise/src/script.js
+++ 11-materials/exercise/src/script.js
@@ -32,7 +32,6 @@ const doorRoughnessTexture = textureLoader.load('./textures/door/roughness.jpg')
 const matcapTexture = textureLoader.load('./textures/matcaps/9.png')
 const gradientTexture = textureLoader.load('./textures/gradients/5.jpg')
 
-
 doorColorTexture.colorSpace = THREE.SRGBColorSpace
 matcapTexture.colorSpace = THREE.SRGBColorSpace
 
@@ -79,7 +78,7 @@ gradientTexture.generateMipmaps = false
 material.gradientMap = gradientTexture */
 
 // MeshStandardMaterial
-const material = new THREE.MeshStandardMaterial()
+/* const material = new THREE.MeshStandardMaterial()
 material.metalness = 1
 material.roughness = 1
 material.map = doorColorTexture
@@ -95,7 +94,69 @@ material.transparent = true
 material.alphaMap = doorAlphaTexture
 
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
-gui.add(material, 'roughness').min(0).max(1).step(0.0001) 
+gui.add(material, 'roughness').min(0).max(1).step(0.0001) */
+
+// MeshPhysicalMaterial
+const material = new THREE.MeshPhysicalMaterial()
+material.metalness = 0
+material.roughness = 0
+/* material.map = doorColorTexture
+material.aoMap = doorAmbientOcclusionTexture
+material.aoMapIntensity = 1
+material.displacementMap = doorHeightTexture
+material.displacementScale = 0.1
+material.metalnessMap = doorMetalnessTexture
+material.roughnessMap = doorRoughnessTexture
+material.normalMap = doorNormalTexture
+material.normalScale.set(0.5, 0.5)
+material.transparent = true
+material.alphaMap = doorAlphaTexture */
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+
+// Clearcoat
+/* material.clearcoat = 1
+material.clearcoatRoughness = 0
+
+gui.add(material, 'clearcoat').min(0).max(1).step(0.0001)
+gui.add(material, 'clearcoatRoughness').min(0).max(1).step(0.0001) */
+
+// Sheen
+/* material.sheen = 1
+material.sheenRoughness = 0.25
+material.sheenColor.set(1, 1, 1)
+
+gui.add(material, 'sheen').min(0).max(1).step(0.0001)
+gui.add(material, 'sheenRoughness').min(0).max(1).step(0.0001)
+gui.addColor(material, 'sheenColor') */
+
+// Iridescence
+/* material.iridescence = 1
+material.iridescenceIOR = 1
+material.iridescenceThicknessRange = [100, 800]
+
+gui.add(material, 'iridescence').min(0).max(1).step(0.0001)
+gui.add(material, 'iridescenceIOR').min(0).max(2.333).step(0.0001)
+gui.add(material.iridescenceThicknessRange, '0').min(1).max(1000).step(1)
+gui.add(material.iridescenceThicknessRange, '1').min(1).max(1000).step(1) */
+
+// Transmission
+material.transmission = 1
+material.ior = 1.5
+material.thickness = 0.5
+
+gui.add(material, 'transmission').min(0).max(1).step(0.0001)
+gui.add(material, 'ior').min(0).max(10).step(0.0001)
+gui.add(material, 'thickness').min(0).max(1).step(0.0001)
+
+
+
+
+
+
+
+
 
 
 const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.15, 64, 64), material);
@@ -160,11 +221,35 @@ const camera = new THREE.PerspectiveCamera(
   75,
   sizes.width / sizes.height,
   0.1,
-  1000
+  100
 );
 camera.position.z = 4;
+scene.add(camera);
 
-lane.rotation.y = 0.1 * elapsedTime
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime
+  plane.rotation.y = 0.1 * elapsedTime
   torus.rotation.y = 0.1 * elapsedTime
 
   sphere.rotation.x = - 0.15 * elapsedTime
